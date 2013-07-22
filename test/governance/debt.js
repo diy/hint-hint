4cbcc052bb83c9c112e45fbb9115c339544a66ca
Zach Bruggeman <zbruggeman@me.com>
2013-07-22 14:16:30 -0700

    Allow for multiple paths in one function call

--- test/governance/debt.js
+++ test/governance/debt.js
@@ -5,7 +5,7 @@ bux(__dirname + '/../../lib/hint-hint.js', function (err, obj) {
     test('debt', function (t) {
         t.equal(err, null, 'Errors should be null');
         t.type(obj, 'number', 'Results should be a number');
-        t.ok(obj > 80, 'Remaining bux should be greater than 80');
+        t.ok(obj > 90, 'Remaining bux should be greater than 90');
         t.end();
     });
 });
